8eff1030c22846d0007490b9dc932fcdc5b03406
PhiHongThai <vutran@192.168.68.112>
2026-05-23 07:49:33 +0700

    fix bug avatar click

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -10,17 +10,23 @@ function Dialog({ ...props }: DialogPrimitive.Root.Props) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />
 }
 
-function DialogTrigger({ ...props }: DialogPrimitive.Trigger.Props) {
-  return <DialogPrimitive.Trigger data-slot="dialog-trigger" {...props} />
-}
+const DialogTrigger = React.forwardRef<
+  HTMLButtonElement,
+  DialogPrimitive.Trigger.Props
+>(({ ...props }, ref) => {
+  return <DialogPrimitive.Trigger ref={ref} data-slot="dialog-trigger" {...props} />
+})
+DialogTrigger.displayName = "DialogTrigger"
 
-function DialogPortal({ ...props }: DialogPrimitive.Portal.Props) {
-  return <DialogPrimitive.Portal data-slot="dialog-portal" {...props} />
-}
+const DialogPortal = DialogPrimitive.Portal
 
-function DialogClose({ ...props }: DialogPrimitive.Close.Props) {
-  return <DialogPrimitive.Close data-slot="dialog-close" {...props} />
-}
+const DialogClose = React.forwardRef<
+  HTMLButtonElement,
+  DialogPrimitive.Close.Props
+>(({ ...props }, ref) => {
+  return <DialogPrimitive.Close ref={ref} data-slot="dialog-close" {...props} />
+})
+DialogClose.displayName = "DialogClose"
 
 function DialogOverlay({
   className,
